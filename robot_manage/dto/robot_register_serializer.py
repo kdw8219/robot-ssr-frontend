ad11e569073c06eb7d3587e96981d2ff0cf30ab0
kdw8219 <kdw8219@gmail.com>
2025-11-17 17:29:37 +0900

    Feature  - Add Robot Secret(sort of password) field and process

--- robot_manage/dto/robot_register_serializer.py
+++ robot_manage/dto/robot_register_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 class RobotRegisterSerializer(serializers.Serializer):
     robot_id = serializers.CharField(max_length=128)
+    robot_secret = serializers.CharField(max_length=256)
     model = serializers.CharField(max_length=64)
     firmware_version = serializers.CharField(max_length=64)
     location = serializers.CharField(max_length=128)
