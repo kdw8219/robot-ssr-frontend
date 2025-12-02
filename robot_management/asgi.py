18954e5f1449acaa9da15225f4200b5e52d89512
kdw8219 <kdw8219@gmail.com>
2025-12-02 16:19:13 +0900

    Feature  - add streaming, robot control related services    -> add redis connect to manage websocket channel, if there are many django ssr.

--- robot_management/asgi.py
+++ robot_management/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import streaming.router
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robot_management.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(streaming.router.websocket_urlpatterns)
+    )
+})
