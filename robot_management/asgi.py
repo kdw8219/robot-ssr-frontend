71561c414ba2111f859a0470c3c6b9ad21e8a02c
kdw8219 <kdw8219@gmail.com>
2026-01-05 11:05:52 +0900

    Feature  - Delete Unnecessary Logic(streaming)   --> this logic is alternated to rust based server

--- robot_management/asgi.py
+++ robot_management/asgi.py
@@ -10,15 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import streaming.router
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robot_management.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(streaming.router.websocket_urlpatterns)
-    )
-})
+application = get_asgi_application()
